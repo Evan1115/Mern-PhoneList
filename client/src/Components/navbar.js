b8f4a4d4ee9ff83bb276e41373dbe9a2f9ff6cf4
Joram <jing111597@gmail.com>
2021-05-19 20:28:57 +0800

    app title changed

--- client/src/Components/navbar.js
+++ client/src/Components/navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-dark bg-dark navbar-expand-lg p-3">
             <div className="container">
-                <Link to="/" className="navbar-brand">Contact List</Link>
+                <Link to="/" className="navbar-brand">Phone List</Link>
             </div>
 
 
